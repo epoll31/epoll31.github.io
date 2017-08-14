6be572609e0b4c85a304bb70b1a03922a54aadb0
ETHAN POLLACK <epollack31@gmail.com>
2017-08-13 20:51:02 -0700

    Added classes and made a main menu

--- javascript.js
+++ javascript.js
@@ -1,31 +1,168 @@
 "use strict";
 
+
 var canvas;
 var graphics;
 
-var sprite;
+var ball;
+var leftPaddle;
+var rightPaddle;
 
-window.onload = () => 
-{
+var pong;
+var snakeButton;
+var back;
+
+var state;
+
+var updateInterval;
+var drawInterval;
+
+var snake;
+
+window.onload = () => {
     canvas = document.getElementsByTagName("canvas")[0];
     graphics = canvas.getContext("2d");
 
-    sprite = new BouncySprite(50, 50, 100, 100, 2, "Red");
+    ball = new BouncySprite((canvas.width / 2) - 12.5, (canvas.height / 2) - 12.5, 25, 25, 4, "Black");
+    leftPaddle = new Paddle(15, canvas.height / 2 - 50, 25, 100, "Red");
+    rightPaddle = new Paddle(canvas.width - 40, canvas.height / 2 - 50, 25, 100, "Red");
 
-    setInterval(Update, 30);
-    setInterval(Draw, 30);
+    pong = new Button(50, 50, 150, 50, "Pong", 32.5, 12.5, 40, "Red", "Black");
+    snakeButton = new Button(225, 50, 150, 50, "Snake", 20, 10, 40, "Red", "Black");
+    back = new Button(25, 25, 75, 25, "Back", 15, 5, 20, "Red", "Black");
+
+    state = 0;
+
+    snake = new Snake(canvas.width/2 - 7.5, canvas.height/2 - 7.5, 15, "Black");
+
+    updateInterval = setInterval(Update, 30);
+    drawInterval = setInterval(Draw, 30);
+
+    canvas.addEventListener("mousedown", mouseClickPosition, false);
+    window.addEventListener("keydown", keyDown, false);
 }
 
+function Update() {
+    if (state == 1) {
+        ball.Update();
+        rightPaddle.y = ball.y - (rightPaddle.height / 2) + (ball.height / 2);
 
-function Update()
-{  
-    sprite.Update();
+        if (rightPaddle.y <= 15) {
+            rightPaddle.y = 15;
+        }
+        else if (rightPaddle.y + rightPaddle.height + 15 >= canvas.height) {
+            rightPaddle.y = canvas.height - rightPaddle.height - 15;
+        }
+
+        //console.log(ball.x, leftPaddle.x + leftPaddle.width);
+        if ((ball.y > leftPaddle.y && ball.y + ball.height < leftPaddle.y + leftPaddle.height) && ball.x <= leftPaddle.x + leftPaddle.width) {
+            ball.x = leftPaddle.x + leftPaddle.width;
+            ball.xSpeed *= -1;
+        }
+        else if (ball.x <= 0) {
+            alert("Game Over!");
+            ResetBall();
+            leftPaddle.y = (canvas.height / 2) - leftPaddle.height / 2;
+        }
+
+        if (ball.x + ball.width >= rightPaddle.x) {
+            ball.x = rightPaddle.x - ball.width;
+            ball.xSpeed *= -1;
+        }
+    }
+    else if (state == 2) {
+        snake.Update();
+    }
 }
 
-function Draw()
-{
+function Draw() {
     graphics.fillStyle = "CornflowerBlue";
     graphics.fillRect(0, 0, canvas.width, canvas.height);
+    if (state == 0) {
+        pong.Draw();
+        snakeButton.Draw();
+    }
+    else if (state == 1) {
+        back.Draw();
+        ball.Draw();
+        leftPaddle.Draw(graphics);
+        rightPaddle.Draw(graphics);
+    }
+    else if (state == 2) {
+        back.Draw();
+        snake.Draw();
+    }
+}
 
-    sprite.Draw(graphics);
+function ResetBall() {
+    ball.x = canvas.width / 2 + ball.width / 2;
+    ball.y = canvas.height / 2 + ball.height / 2;
+    ball.SpeedX = (Math.floor((Math.random() * 2) + 0) - 1) * 2;
+    ball.SpeedY = (Math.floor((Math.random() * 2) + 0) - 1) * 2;
+}
+
+
+function mouseClickPosition(event) {
+    var x = event.x - canvas.offsetLeft;
+    var y = event.y - canvas.offsetTop;
+
+    if (state == 0) {
+        if (pong.Clicked(x, y)) {
+            clearInterval(updateInterval);
+            updateInterval = setInterval(Update, 30);
+            clearInterval(drawInterval);
+            drawInterval = setInterval(Draw, 30);
+            state = 1;
+        }
+        else if (snakeButton.Clicked(x, y)) {
+            clearInterval(updateInterval);
+            updateInterval = setInterval(Update, 150);
+            clearInterval(drawInterval);
+            drawInterval = setInterval(Draw, 150);
+            state = 2;
+        }
+    }
+    else if (state != 0 && back.Clicked(x, y)) {
+        clearInterval(updateInterval);
+        updateInterval = setInterval(Update, 30);
+        clearInterval(drawInterval);
+        drawInterval = setInterval(Draw, 30);
+        state = 0;
+    }
+}
+
+function keyDown(event) {
+    switch (event.keyCode) {
+        case 37:
+            if (state == 2) {
+                snake.ChangeDirection(2);
+            }
+            break;
+        case 39:
+            if (state == 2) {
+                snake.ChangeDirection(0);
+            }
+            break;
+        case 38:
+            if (state == 1) {
+                leftPaddle.MoveUp();
+            }
+            else if (state == 2) {
+                snake.ChangeDirection(3);
+            }
+            break;
+        case 40:
+            if (state == 1) {
+                leftPaddle.MoveDown();
+            }
+            else if (state == 2) {
+                snake.ChangeDirection(1);
+            }
+            break;
+        case 32:
+            if (state == 2){
+                snake.AddPiece();
+            }
+            break;
+    }
 }
--- mainClasses.js
+++ mainClasses.js
@@ -1,6 +1,5 @@
-class Sprite{
-    constructor(x = number, y = number, width = number, height = number, color)
-    {
+class Sprite {
+    constructor(x = number, y = number, width = number, height = number, color) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -8,8 +7,7 @@ class Sprite{
         this.color = color;
     }
 
-    Draw()
-    {   
+    Draw() {
         graphics.beginPath();
         graphics.fillStyle = this.color;
         graphics.fillRect(this.x, this.y, this.width, this.height);
@@ -18,31 +16,26 @@ class Sprite{
     }
 }
 
-class BouncySprite extends Sprite{
-    constructor(x = number, y = number, width = number, height = number, speed = number, color){
-        super (x, y, width, height, color);
+class BouncySprite extends Sprite {
+    constructor(x = number, y = number, width = number, height = number, speed = number, color) {
+        super(x, y, width, height, color);
 
         this.xSpeed = speed;
         this.ySpeed = speed;
     }
 
-    Update()
-    {
-        if (this.x + this.width >= canvas.width)
-        {
+    Update() {
+        if (this.x + this.width >= canvas.width) {
             this.xSpeed *= -1;
         }
-        else if (this.x <= 0)
-        {
+        else if (this.x <= 0) {
             this.xSpeed *= -1;
         }
-    
-        if (this.y + this.height >= canvas.height)
-        {
+
+        if (this.y + this.height >= canvas.height) {
             this.ySpeed *= -1;
         }
-        else if (this.y <= 0)
-        {
+        else if (this.y <= 0) {
             this.ySpeed *= -1;
         }
 
@@ -51,8 +44,122 @@ class BouncySprite extends Sprite{
     }
 }
 
-class Paddle extends Sprite{
-    constructor (x = number, y = number, width = number, height = number, color) {
-        super (x, y, width, height, color);
+class Paddle extends Sprite {
+    constructor(x = number, y = number, width = number, height = number, color) {
+        super(x, y, width, height, color);
+    }
+
+    MoveUp() {
+        if (this.y > 15) {
+            this.y -= 5;
+        }
+    }
+    MoveDown() {
+        if (this.y + this.height + 15 < canvas.height) {
+            this.y += 5;
+        }
+    }
+}
+
+class TextSprite {
+    constructor(x = number, y = number, text = string, size, color) {
+        this.x = x;
+        this.y = y;
+        this.text = text;
+        this.size = size;
+        this.color = color;
+    }
+
+    Draw() {
+        graphics.beginPath();
+        graphics.fillStyle = this.color;
+        graphics.font = this.size + "px arial";
+        graphics.fillText(this.text, this.x, this.y)
+        graphics.closePath();
+    }
+}
+
+class Button {
+    constructor(x = number, y = number, width = number, height = number, text = string, xOffset = number, yOffest = number, textSize, backgroundColor, textColor) {
+        this.sprite = new Sprite(x, y, width, height, backgroundColor);
+        this.text = new TextSprite(x + xOffset, y + height - yOffest, text, textSize, textColor);
+    }
+
+    Clicked(x = number, y = number) {
+        return ((x > this.sprite.x && x < this.sprite.x + this.sprite.width) && (y > this.sprite.y && y < this.sprite.y + this.sprite.height));
+    }
+
+    Draw() {
+        this.sprite.Draw();
+        this.text.Draw();
+    }
+}
+
+class Snake {
+    constructor(x = number, y = number, size = number, color) {
+        this.direction = 0;
+        this.body = [new SnakePiece(x, y, size, 0, color, 0)];
+    }
+
+    Update() {
+        console.log(this.body.length);
+        this.body[0].Move();
+        for (var i = 1; i < this.body.length; i++) {
+            var piece = this.body[i];
+
+            piece.direction = this.body[i - 1].direction;
+
+            piece.Move();
+        }
+    }
+
+    Draw() {
+        for (var i = 0; i < this.body.length; i++) {
+            var piece = this.body[i];
+
+            piece.Draw();
+        }
+    }
+
+    AddPiece() {
+        var newPiece;
+        if (this.body.length > 0) {
+            newPiece = this.body[this.body.length - 1];
+            newPiece = new SnakePiece(newPiece.x, newPiece.y, newPiece.width, newPiece.height, newPiece.color, newPiece.direction);
+        }
+        this.body[this.body.length] = newPiece;
+    }
+
+    ChangeDirection(directionChange = number) {
+        if (((directionChange == 0 && this.body[0].direction == 2) ||
+            (directionChange == 1 && this.body[0].direction == 3) ||
+            (directionChange == 2 && this.body[0].direction == 0) ||
+            (directionChange == 3 && this.body[0].direction == 1))) { return; }
+
+        this.body[0].direction = directionChange;
+    }
+}
+
+class SnakePiece extends Sprite {
+    constructor(x = number, y = number, size = number, direction = number, color) {
+        super(x, y, size, size, color);
+        this.direction = direction;
+    }
+
+    Move() {
+        switch (this.direction) {
+            case 0:
+                this.x += this.width + this.width / 7.5;
+                break;
+            case 1:
+                this.y += this.height + this.height / 7.5;
+                break;
+            case 2:
+                this.x -= this.width + this.width / 7.5;
+                break;
+            case 3:
+                this.y -= this.height + this.height / 7.5;
+                break;
+        }
     }
 }
