42a644ec36eed981979258b848df4007e1078994
ethan <epollack31@gmail.com>
2023-12-28 12:04:43 -0500

    added new page for mqp details

---
+++ src/app/mqp/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '../globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Ethan Pollack | MQP',
+  description: 'Major Qualifying Project for Ethan Pollack',
+}
+
+export default function MQPLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <section>
+      {children}
+    </section>
+  )
+}
---
+++ src/app/mqp/page.tsx
@@ -0,0 +1,8 @@
+
+
+export default function MQP() {
+
+  return (
+    <div>test</div>
+  );
+}
