aa9612a515ad27f5635f4b5c45151241716666bc
ethan <epollack31@gmail.com>
2023-12-28 20:46:26 -0500

    cleaned up page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,26 +19,15 @@ export default async function Home() {
         <h1 className="text-5xl py-2">Ethan Pollack</h1>
         <h2 className="text-l">Software Developer · Designer · Etc.</h2>
       </div>
+      
       <div className="flex flex-row gap-5 text-xl" >
-        
         <a className="underline-offset-0 hover:underline hover:underline-offset-4 transition-all" href="#projects">Projects</a>
         <h1 className="">·</h1>
         <a className="underline-offset-0 hover:underline hover:underline-offset-4 transition-all" href="#courses">Courses</a>
         <h1 className="">·</h1>
         <a className="underline-offset-0 hover:underline hover:underline-offset-4 transition-all" href="#resume">Resume</a>
       </div>
-      {/* <div className="flex flex-row flex-wrap w-full p-5 justify-evenly gap-2" id="courses">
-        <h1 className="flex-grow w-full text-2xl text-center" >Courses Taken</h1>
-        {
-          courses.map((c: Course) => <CourseCard course={c} className="flex-grow" />)
-        }
-      </div> */}
-      {/* <div className="flex flex-row flex-wrap w-full  p-5 justify-evenly gap-2">
-        <h1 className="flex-grow w-full text-2xl text-center" id="projects">Project Experience</h1>
-        {
-          projects.map((p: Project) => <ProjectCard project={p} className="flex-grow" key={p.name}/>)
-        }
-      </div> */}
+
       <ProjectsSection projects={projects}/>
       <CoursesSection courses={courses}/>
       <div className="flex flex-row flex-wrap w-full  p-5 justify-evenly gap-2">
