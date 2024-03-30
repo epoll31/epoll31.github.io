9bd689f41145ba715f2d085650d9ebbcdffae6f8
ethan <epollack31@gmail.com>
2024-03-30 16:19:09 -0700

    remove all config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
   // basePath: '',
-  output: 'export',
+  // output: 'export',
   // images: {
   //   unoptimized: false
   // },
