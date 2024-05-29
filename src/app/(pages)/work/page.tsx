557ff88d9aa3f7a26405b5fd3fad51122a6f49c5
ethan <epollack31@gmail.com>
2024-05-29 10:28:06 -0700

    referencing local resume now

--- src/app/(pages)/work/page.tsx
+++ src/app/(pages)/work/page.tsx
@@ -11,20 +11,26 @@ export default function WorkPage() {
   const mediaSize = useMediaSizes();
 
   return (
-    <Page tabs={["Projects", "Education", "Early Years"]} >
-      {
-        !smOrSmaller(mediaSize) &&
-        (
-          <PageMajor type="Side" className={` mt-10 `} >
-            <CardContainer containerClassName="p-0" className="overflow-hidden drop-shadow-2xl">
-              <Link href="https://raw.githubusercontent.com/epoll31/Resume/main/resume.pdf" className="w-full h-fit">
-                <img src="/images/resume-color.png" alt="resume" width={200} height={200} className="w-full" />
-              </Link>
-            </CardContainer>
-          </PageMajor>
-        )
-      }
+    <Page tabs={["Projects", "Education", "Early Years"]}>
+      {!smOrSmaller(mediaSize) && (
+        <PageMajor type="Side" className={` mt-10 `}>
+          <CardContainer
+            containerClassName="p-0"
+            className="overflow-hidden drop-shadow-2xl"
+          >
+            <Link href="./resume.pdf" className="w-full h-fit">
+              <img
+                src="/images/resume-color.png"
+                alt="resume"
+                width={200}
+                height={200}
+                className="w-full"
+              />
+            </Link>
+          </CardContainer>
+        </PageMajor>
+      )}
       <Work />
     </Page>
   );
-};
+}
